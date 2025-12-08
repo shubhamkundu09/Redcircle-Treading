1b78f64572d118b2d38554ec8c881272b54e57a3
shubham <shubhamkundu1122@gmail.com>
2025-12-08 09:58:49 +0530

    Changes

--- src/main/java/com/redcircle/config/JwtProvider.java
+++ src/main/java/com/redcircle/config/JwtProvider.java
@@ -1,4 +1,23 @@
 package com.redcircle.config;
 
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.crypto.SecretKey;
+import java.util.Collection;
+
 public class JwtProvider {
+
+    public static SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
+    public static String generateToken(Authentication auth){
+        Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+        String roles = populateAuthorities(authorities);
+        return null;
+    }
+
+    private static String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        return null;
+    }
 }
